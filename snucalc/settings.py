3166d6eb929fa56655271b5f5d13ad03f30660e8
repositivator <repositivator@gmail.com>
2017-01-17 16:21:59 +0900

    Add an app named calcmain

--- snucalc/settings.py
+++ snucalc/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
