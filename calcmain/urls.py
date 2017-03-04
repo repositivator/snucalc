c4ce6df6634061cac47147939ba35b556838920e
repositivator <repositivator@gmail.com>
2017-03-04 20:45:50 +0900

    Add contact-us button on nav-bar and apply iframe for email form

--- calcmain/urls.py
+++ calcmain/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from django.conf.urls import url
 from . import views
 
@@ -12,4 +13,6 @@ urlpatterns = [
     url(r'^data_reassessment2/(?P<pk>\d+)/$', views.data_reassessment2, name="data_reassessment2"),
     url(r'^final_result/(?P<pk>\d+)/$', views.final_result, name="final_result"),
     url(r'^export_delete/(?P<pk>\d+)/$', views.export_delete, name="export_delete"),
+    url(r'^contact_us/', TemplateView.as_view(template_name="calcmain/contact_us.html"), name='contact_us'),
+    url(r'^mail_complete/', TemplateView.as_view(template_name="calcmain/mail_complete.html"), name='mail_complete'),
 ]
